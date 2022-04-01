aa583dca3e8432f49e85699006208a87ebae6c33
124tranvita <124tranvita@gmail.com>
2022-04-01 11:45:34 +0700

    update

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -21,7 +21,7 @@ function Menu({ dishes }) {
       {Object.keys(selectedDish).length !== 0 &&
         <div className="row">
           <div className='col-12'>
-            <Card style={{ width: '50%' }} className='mx-auto'>
+            <Card className='mx-auto' style={{ width: '80%' }}>
               <Card.Img src={selectedDish.image} alt={selectedDish.name} />
               <Card.ImgOverlay>
                 <Card.Title>{selectedDish.name}</Card.Title>
