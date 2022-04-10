39f27203d49147c955c79fd78d643a4339ea9c2c
124tranvita <124tranvita@gmail.com>
2022-04-11 01:13:56 +0700

    Basic function done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color='primary'>
-        <div className='container'>
-          <NavbarBrand href='/'>Ristorante Con Fusion</NavbarBrand>
-        </div>
-      </Navbar>
-      <Menu dishes={DISHES} />
+      <Main />
     </div>
   );
 }
--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,62 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Card, CardImg, CardTitle, CardBody, CardText } from 'reactstrap';
-import { COMMENTS } from '../shared/comments';
 
-class DishDetail extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      comments: COMMENTS
-    }
-  }
-
-  renderSelectedDish() {
-    if (this.props.selectedDish != null)
-      return (
-        <Card>
-          <CardImg top src={this.props.selectedDish.image} alt={this.props.selectedDish.name} />
-          <CardBody>
-            <CardTitle>{this.props.selectedDish.name}</CardTitle>
-            <CardText>{this.props.selectedDish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    else
-      return (
-        <></>
-      );
-  }
-
-  renderComments() {
-    const comment = this.state.comments.map((comment) => {
-      return (
-        <div key={comment.id}>
-          <p>{comment.content}</p>
-          <p>-- <span>{comment.username}</span>, <span>{comment.date}</span></p>
-        </div>
-      )
-    })
-    if (this.props.selectedDish != null)
-      return comment;
-    else
-      return (
-        <></>
-      )
-  }
-
-  render() {
+function RenderDish({ dish }) {
+  if (dish != null)
     return (
-      <div className='row' >
-        <div className='col-12 col-md-5 mt-5'>
-          {this.renderSelectedDish()}
-        </div>
-        <div className='col-12 col-md-5 mt-5' style={{ textAlign: 'left' }}>
-          {(this.props.selectedDish != null) && (<h3>Comment</h3>)}
-          {this.renderComments()}
-        </div>
+      <Card>
+        <CardImg top src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  else
+    return (
+      <></>
+    )
+}
+
+function RenderComment({ comments }) {
+  const comment = comments.map((comment) => {
+    return (
+      <div key={comment.id}>
+        <p>{comment.content}</p>
+        <p>-- <span>{comment.username}</span>, <span>{comment.date}</span></p>
       </div>
     )
-  }
+  });
+
+  return comment;
+}
+
+const DishDetail = (props) => {
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-12 col-md-5 m-1'>
+          <RenderDish dish={props.dish} />
+        </div>
+        <div className='col-12 col-md-5 m-1' style={{ textAlign: 'left' }}>
+          {props.dish != null && (
+            <>
+              <h3>Comment</h3>
+              <RenderComment comments={props.comments} />
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default DishDetail
