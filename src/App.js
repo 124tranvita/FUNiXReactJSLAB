8d1ffc925bee0bfd6b712d01e653cc8bc1bb8ec0
124tranvita <124tranvita@gmail.com>
2022-04-13 23:22:59 +0700

    Complete Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Main from './components/MainComponent';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Main />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import reportWebVitals from './reportWebVitals';
 
 // Root container
