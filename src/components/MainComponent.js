79b2f0e84e8467cd85f88079595880f8e38a191f
124tranvita <124tranvita@gmail.com>
2022-04-14 14:32:02 +0700

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,14 +5,20 @@ import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
+import Contact from "./ContactComponent";
 import { DISHES } from '../shared/dishes';
-import { COMMENTS } from '../shared/comments'
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       dishes: DISHES,
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
       selectedDish: null
     };
   }
@@ -23,9 +29,11 @@ class Main extends Component {
 
   render() {
     const HomePage = () => {
-      return (
-        <Home />
-      )
+      return (<Home
+        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+      />)
     };
 
     return (
@@ -33,7 +41,13 @@ class Main extends Component {
         <Header />
         <Switch>
           <Route path="/home" component={HomePage} />
-          <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelected(dishId)} />} />
+          <Route
+            exact
+            path="/menu"
+            component={() => <Menu
+              dishes={this.state.dishes}
+              onClick={(dishId) => this.onDishSelected(dishId)} />} />
+          <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
 
